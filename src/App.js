63776cf802965adbc0c653a9b40c12ec8053f295
Akash Sharma <sharma.akash221@gmail.com>
2023-07-24 18:30:14 -0400

    Landing Page feature #5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen />
+      <BrowserRouter>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />}></Route>
+          </Routes>
+        )}
+      </BrowserRouter>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -19,15 +19,19 @@ function Row({ title, fetchURL, isLargeRow = false }) {
     <div className="row">
       <h2>{title}</h2>
       <div className="row_posters">
-        {movies.map((movie) => (
-          <img
-            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
       </div>
     </div>
   );
