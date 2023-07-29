14ac699897fc041adce35d3bcfa624eec79a8654
Akash Sharma <sharma.akash221@gmail.com>
2023-07-29 19:35:41 -0400

    state management with redux  Feature#7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "./firebase";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
       <BrowserRouter>
