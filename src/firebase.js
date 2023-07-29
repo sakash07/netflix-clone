80223a8947046fd39ae09b5ea628de39c0956ebe
Akash Sharma <sharma.akash221@gmail.com>
2023-07-28 23:27:05 -0400

    Login page Feature#6

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyA07Make5tI5oM95jsVn7v2U3U0FDxQ4_c",
   authDomain: "netflix-clone-93c41.firebaseapp.com",
@@ -6,3 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "432812357243",
   appId: "1:432812357243:web:e4ffa4ffa07ab0f28e14de",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./LoginScreen.css";
+import SignUpScreen from "./SignUpScreen";
 
 function LoginScreen() {
   const [signIn, setSignIn] = useState(false);
@@ -17,26 +18,30 @@ function LoginScreen() {
         <div className="loginScreen_gradient" />
       </div>
       <div className="loginScreen_body">
-        <>
-          <h1>Unlimited films, TV Programs and more</h1>
-          <h2>Watch anywhere, Cancel at any time.</h2>
-          <h3>
-            Ready to watch? Enter your email to create or restart your
-            membership
-          </h3>
+        {signIn ? (
+          <SignUpScreen />
+        ) : (
+          <>
+            <h1>Unlimited films, TV Programs and more</h1>
+            <h2>Watch anywhere, Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </h3>
 
-          <div className="loginScreen_input">
-            <form>
-              <input type="email" placeholder="Email Address" />
-              <button
-                onClick={() => setSignIn(true)}
-                className="loginScreen_getStarted"
-              >
-                Get Started
-              </button>
-            </form>
-          </div>
-        </>
+            <div className="loginScreen_input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen_getStarted"
+                >
+                  Get Started
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
